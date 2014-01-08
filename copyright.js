dc942783011d0a0544afdf56d544568a07d191d7
Richard Cornish <richardcornish@gmail.com>
2014-01-07 20:23:36 -0600

    Added error handling if no element has an ID of ...this is why you use jQuery

--- copyright.js
+++ copyright.js
@@ -1,31 +1,41 @@
-(function(){
+(function () {
 
-  var Copyright = function(config){
+  'use strict';
+
+  var Copyright = function (config) {
     this.config = config;
     this.setup();
   };
 
-  Copyright.prototype.setup = function(){
+  Copyright.prototype.setup = function () {
     this.insertText();
   };
 
-  Copyright.prototype.currentYear = function(){
+  Copyright.prototype.currentYear = function () {
     return new Date().getFullYear();
   };
 
-  Copyright.prototype.owner = function(){
-    return document.querySelectorAll(this.config.crSelector)[0].getAttribute('data-owner');
+  Copyright.prototype.getElement = function () {
+    return document.querySelectorAll(this.config.crSelector)[0];
   };
 
-  Copyright.prototype.insertText = function(){
+  Copyright.prototype.owner = function () {
+    if (this.getElement()) {
+      this.getElement().getAttribute('data-owner');
+    }
+  };
+
+  Copyright.prototype.insertText = function () {
     var text = "Copyright \u00A9 " + this.currentYear() + " " + this.owner();
-    document.querySelectorAll(this.config.crSelector)[0].innerHTML = text;
+    if (this.getElement()) {
+      this.getElement().innerHTML = text;
+    }
   };
 
-  document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function () {
     var c = new Copyright({
       crSelector: '#plugin-copyright'
     });
   }, false);
 
-})();
+}());
